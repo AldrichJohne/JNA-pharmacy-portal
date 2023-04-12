4b030d6ca4e3b3c68401f2c2a80f0750c6365756
Aldrich Johne Ulabo <you@example.com>
2023-04-12 11:05:56 +0800

    added reporting of gross and profit by date range

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 import {SaleDialogComponent} from "./components/sale-dialog/sale-dialog.component";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import * as moment from 'moment';
+import {SaleReportsService} from "./services/sale-reports.service";
 
 @Component({
   selector: 'app-root',
@@ -14,22 +17,31 @@ import {SaleDialogComponent} from "./components/sale-dialog/sale-dialog.componen
 })
 export class AppComponent implements OnInit{
   title = 'product-system-v1';
+  dateRangeReportForm!: FormGroup;
+  reportDateRangeValue: string = "";
+  reportGrossValue: string = "";
+  reportProfitValue: string = "";
 
   displayedColumnsProducts: string[] = ['cashier', 'name', 'classification', 'remainingStock', 'totalStock', 'sold', 'pricePerPc', 'srpPerPc', 'totalPriceRemaining', 'totalPriceSold', 'profit', 'expiryDate', 'action'];
   displayedColumnsSales: string[] = ['name','classification','price','srp','sold','amount','profit','transactionDate'];
   dataSourceProducts!: MatTableDataSource<any>;
   dataSourceSales!: MatTableDataSource<any>;
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild('productsPaginator') productsPaginator!: MatPaginator;
+  @ViewChild('salesPaginator') salesPaginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private dialog : MatDialog,
-              private productService: ProductService) {
-
+              private productService: ProductService,
+              private formBuilder : FormBuilder,
+              private reportService: SaleReportsService) {
   }
   ngOnInit(): void {
     this.getAllProductList();
     this.getAllSales();
+    this.dateRangeReportForm = this.formBuilder.group({
+      startDateTemp: ['',Validators.required],
+      endDateTemp: ['',Validators.required]
+    });
   }
 
   openProductDialog() {
@@ -60,7 +72,7 @@ export class AppComponent implements OnInit{
       .subscribe({
       next:(res)=>{
         this.dataSourceProducts = new MatTableDataSource(res);
-        this.dataSourceProducts.paginator = this.paginator;
+        this.dataSourceProducts.paginator = this.productsPaginator;
         this.dataSourceProducts.sort = this.sort;
       },
       error:()=>{
@@ -74,8 +86,8 @@ export class AppComponent implements OnInit{
       .subscribe({
         next:(res)=>{
           this.dataSourceSales = new MatTableDataSource(res);
-          this.dataSourceProducts.paginator = this.paginator;
-          this.dataSourceProducts.sort = this.sort;
+          this.dataSourceSales.paginator = this.salesPaginator;
+          this.dataSourceSales.sort = this.sort;
         },
         error:()=>{
           alert("Error While Fetching The Product Sales")
@@ -119,6 +131,25 @@ export class AppComponent implements OnInit{
     })
   }
 
+  findReportRecord() {
+    this.dateRangeReportForm.enable();
+    const convertedStartDate = moment(this.dateRangeReportForm.value.startDateTemp).format('YYYY-MM-DD');
+    const convertedEndDate = moment(this.dateRangeReportForm.value.endDateTemp).format('YYYY-MM-DD');
+    this.reportService.getReportByDateRange(convertedStartDate, convertedEndDate).subscribe(
+      {
+        next:(res)=>{
+          this.reportDateRangeValue = res.date;
+          this.reportGrossValue = res.gross;
+          this.reportProfitValue = res.profit;
+          this.dateRangeReportForm.reset();
+        },
+        error:()=>{
+          alert("Error while fetching reports by given date range: " + convertedStartDate + " to " + convertedEndDate);
+        }
+      }
+    )
+  }
+
   applyFilterProducts(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSourceProducts.filter = filterValue.trim().toLowerCase();
@@ -127,6 +158,7 @@ export class AppComponent implements OnInit{
       this.dataSourceProducts.paginator.firstPage();
     }
   }
+
 
   applyFilterSales(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
